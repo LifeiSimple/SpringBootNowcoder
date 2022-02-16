45593141ebe681847814f57df3735aa0af3cbad8
wangjx <wangjiaxiang97@foxmail.com>
2022-02-16 21:09:15 +0800

    配置环境

---
+++ src/main/java/com/nowcoder/community/dao/Student.java
@@ -0,0 +1,7 @@
+package com.nowcoder.community.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+}
--- src/test/java/com/nowcoder/community/NowcoderApplicationTests.java
+++ src/test/java/com/nowcoder/community/NowcoderApplicationTests.java
@@ -1,13 +1,27 @@
 package com.nowcoder.community;
 
+import com.nowcoder.community.dao.Student;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-class NowcoderApplicationTests {
+@ContextConfiguration(classes = NowcoderApplication.class)
+class NowcoderApplicationTests implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
     @Test
-    void contextLoads() {
+    public void testApplicationContext() {
+        System.out.println(applicationContext.getBean(Student.class));
     }
 
 }
