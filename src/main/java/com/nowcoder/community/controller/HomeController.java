15d53d6dcd35e3f74327dd8bbece0119e27428f8
wangjx <wangjiaxiang97@foxmail.com>
2022-02-17 11:49:48 +0800

    首页

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,10 +26,13 @@ public class HomeController {
     private UserService userService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model) {
+    public String getIndexPage(Model model, Page page) {
+        // 方法调用前,SpringMVC会自动实例化Model和Page,并将Page注入Model.
+        // 所以,在thymeleaf中可以直接访问Page对象中的数据.
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
 
-
-        List<DiscussPost> list = discussPostService.findDiscussPosts(0, 0, 10);
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
 
         List<Map<String, Object>> discussPosts = new ArrayList<>();
 
